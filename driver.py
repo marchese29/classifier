ffdae8f17e0116aea2c8af7ceb5244f1feb6c186
jacobs269 <jacobs.269@osu.edu>
2015-09-30 09:36:07 -0400

    Update driver.py

--- driver.py
+++ driver.py
@@ -1,3 +1,26 @@
 from util.crossValidation import crossValidate
 from classifiers import bayes
 
+list256=pickle.load( open( "classifier/data/256WordList.pickle", "rb" ) )
+list1000=pickle.load( open( "classifier/data/1000WordList.pkl", "rb" ) )
+
+matrix1000=pickle.load( open( "classifier/data/reducedFeatureVectorMatrix1000.pkl", "rb" ) )
+matrix256=pickle.load( open( "classifier/data/reducedFeatureVectorMatrix256.pkl", "rb" ) )
+
+trainingTestPairs1000=crossValidate(matrix1000)
+trainingTestPairs256=crossValidate(matrix256)
+
+
+accArray1000=NaiveBayesClassifier(trainingTestPairs1000,list1000)
+accArray256=NaiveBayesClassifier(trainingTestPairs256,list256)
+
+
+
+print("The below line shows 5 accuracies when we use 1000 words.  Each one is for one training/testing fold")
+print(accArray1000)
+
+print("The below line shows 5 accuracies when we use 256 words.  Each one is for one training/testing fold")
+print(accArray256)
+
+
+
